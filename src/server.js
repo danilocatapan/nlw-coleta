f7592ce0acad246cd2f81d625b1c43750ab3a481
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-04 09:43:28 -0300

    feat: add express static public folder

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const server = express()
 
+server.use(express.static("public"))
+
 server.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html")
 })
