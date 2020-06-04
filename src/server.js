3244c24c71a6677e2bbabd52c0d6b96a5cb1b17c
Danilo Rodrigues Catapan <dcatapan@gmail.com>
2020-06-04 10:02:48 -0300

    feat: server GET search-result

--- src/server.js
+++ src/server.js
@@ -10,11 +10,15 @@ nunjucks.configure("src/views", {
 })
 
 server.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html")
+  return res.render("index.html", { title: "Um título"})
 })
 
 server.get("/create-point", (req, res) => {
-  res.sendFile(__dirname + "/views/create-point.html")
+  return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+  return res.render("search-results.html")
 })
 
 server.listen(3000)
